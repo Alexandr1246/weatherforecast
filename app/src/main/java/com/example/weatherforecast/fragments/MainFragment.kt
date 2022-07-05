3654aae4214401815528025134ba1ad6a72acc58
Alex <alex.sh1246@gmail.com>
2022-07-05 20:38:48 +0300

    desing change

--- app/src/main/java/com/example/weatherforecast/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherforecast/fragments/MainFragment.kt
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import com.example.weatherforecast.R
 import com.example.weatherforecast.databinding.FragmentMainBinding
 import com.example.weatherforecast.isPermissionGranted
 
