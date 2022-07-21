6f81d3973e2337e579606eb50aca3a11222cb2f8
Alex <alex.sh1246@gmail.com>
2022-07-21 12:05:06 +0300

    added the requestWeatherData function using the Volley library

--- app/src/main/java/com/example/weatherforecast/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherforecast/fragments/MainFragment.kt
@@ -1,7 +1,9 @@
 package com.example.weatherforecast.fragments
 
 import android.Manifest
+import android.os.Build.VERSION_CODES.N
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +12,14 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherforecast.adapters.ViewPagerAdapter
 import com.example.weatherforecast.databinding.FragmentMainBinding
 import com.example.weatherforecast.isPermissionGranted
 import com.google.android.material.tabs.TabLayoutMediator
-
+const val API_KEY = "0fb7f31f833144f5af9132623220207"
 class MainFragment : Fragment() {
     private val fList = listOf(
         HoursFragment.newInstance(),
@@ -35,6 +40,8 @@ class MainFragment : Fragment() {
 
         checkPermission()
         init()
+        requestWeatherData("Odessa")
+
     }
 
     private fun init() = with(binding){
@@ -59,6 +66,24 @@ class MainFragment : Fragment() {
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun requestWeatherData(city: String){
+
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY&q=$city&days=3&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                    result->Log.d("ErrorLog", "Result: $result")
+            },
+            {
+                error->
+                Log.d("ErrorLog", "Error: $error")
+            }
+        )
+        queue.add(request)
     }
 
     companion object {
